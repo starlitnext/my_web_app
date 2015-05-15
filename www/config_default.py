36c5328176cd6480e00a9bb8b5336bce3ec7e5a0
xiaoqiangxu <vipxxq@foxmail.com>
2015-05-15 14:23:44 +0800

    add day06 configurations

---
+++ www/config_default.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding=utf-8 -*-
+
+'''
+Default configurations.
+'''
+
+__author__ = 'XXQ'
+
+configs = {
+	'db': {
+		'host': '127.0.0.1',
+		'port': 3306,
+		'user': 'www-data',
+		'password': 'www-data',
+		'database': 'awesome'
+	},
+	'session': {
+		'secret': 'AwEsOmE'
+	}
+}
